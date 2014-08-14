2a644b77a40b67d6c0e212d1d517c4c1582b2d5f
hotoo <hotoo.cn@gmail.com>
2014-08-14 15:17:18 +0800

    Update zpmjs init into closure.

--- zpm.js
+++ zpm.js
@@ -3,10 +3,10 @@
   var g_define = global.define;
   var g_zpmjs = global.zpmjs;
 
-  if (!g_zpmjs) {
+  g_zpmjs || function(){
 
-    var zpmjs_factory_cache = {}; // cache factory.
-    var zpmjs_exports_cache = {}; // modules.
+    var zpmjs_factory_cache = {};
+    var zpmjs_exports_cache = {};
 
     var zpmjs_require = function(id){
 
@@ -17,12 +17,11 @@
       if (!factory) { return; }
 
       var module = {
-        exports: {},
-        require: zpmjs_require
+        id: id
       };
 
       zpmjs_exports_cache[id] =
-        factory.call(global, zpmjs_require, module.exports, module) ||
+        factory.call(global, zpmjs_require, module.exports = {}, module) ||
         module.exports;
 
       delete zpmjs_factory_cache[id];
@@ -41,25 +40,24 @@
       }
     };
 
-    var zpm_use = function(module_ids, callback){
-      var mods = [];
-
-      if (!(module_ids instanceof Array)){
-        module_ids = [module_ids];
-      }
-
-      for(var i=0,l=module_ids.length; i<l; i++){
-        mods[i] = zpmjs_require(module_ids[i]);
-      }
-
-      callback.apply(global, mods);
-    };
-
     global.zpmjs = {
-      use: zpm_use,
+      use: function(module_ids, callback){
+        var mods = [];
+
+        if (!(module_ids instanceof Array)){
+          module_ids = [module_ids];
+        }
+
+        for(var i=0,l=module_ids.length; i<l; i++){
+          mods[i] = zpmjs_require(module_ids[i]);
+        }
+
+        callback.apply(global, mods);
+      },
+      require: zpmjs_require,
       define: zpm_define
     };
 
-  }
+  }();
 
 })(this);
